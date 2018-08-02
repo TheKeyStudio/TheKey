2fc88fdf01fa276fe49940f74780f3ce832b21ff
york <york125019@gmail.com>
2018-08-02 16:52:44 +0800

    坤成87

---
+++ TheKeyProject/Assets/Script/拖曳測試/Drag_test.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Fungus;
+public class Drag_test : MonoBehaviour {
+
+    private bool Test1 = false;
+    public bool  Test2 = false;
+
+    public void setTest1T()
+    {
+        Test1 = true;
+    }
+    public void setTest1F()
+    {
+        Test1 = false;
+    }
+    public void setTest2T()
+    {
+        Test2 = true;
+    }
+    public void setTest2F()
+    {
+        Test2 = false;
+    }
+
+    public void Iscorrect()
+    {
+        if (Test1 && Test2)
+        {
+            Flowchart.BroadcastFungusMessage("答對了");
+        }
+        else
+        {
+            Flowchart.BroadcastFungusMessage("答錯了");
+        }
+    }
+
+
+
+}
