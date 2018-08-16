3f257df2219010727a3290cfa319bce26c2d506a
RayFV <rayfvfree@ouklook.com>
2018-08-16 17:08:35 +0800

    updated main1 & added library

---
+++ TheKeyProject/Assets/CandleLight.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CandleLight : MonoBehaviour {
+    private Light light;
+    private Color32 originalColor;
+	// Use this for initialization
+	void Start () {
+        light = GetComponent<Light>();
+        originalColor = light.color;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        int randomRed = Random.Range(originalColor.r, originalColor.r - 20);
+        int randomGreen = Random.Range(originalColor.g, originalColor.g - 20);
+        int randomBlue = Random.Range(originalColor.b, originalColor.b - 20);
+        light.color = new Color32(randomRed, randomGreen, randomBlue, 255);
+	}
+}
---
+++ TheKeyProject/Assets/Script/CameraZoom.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class CameraZoom : MonoBehaviour {
+
+    public GameObject cameraObject;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            cameraObject.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+
+        if (other.CompareTag("Player"))
+        {
+            cameraObject.SetActive(false);
+        }
+    }
+}
