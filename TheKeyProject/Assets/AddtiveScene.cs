dafb761eafdc0372381db86c515a1afa512836e8
RayFV <rayfvfree@ouklook.com>
2018-08-22 16:00:53 +0800

    Remake Book UI
    Use addtiveScene prefab and enter scene name

---
+++ TheKeyProject/Assets/AddtiveScene.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AddtiveScene : MonoBehaviour {
+
+    public string otherSceneName;
+
+	// Use this for initialization
+	void Start () {
+        SceneManager.LoadScene(otherSceneName, LoadSceneMode.Additive);
+    }
+
+}
