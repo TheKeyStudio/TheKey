df6554313235633382e98fdcf4bf4d696cfdf519
york <york125019@gmail.com>
2018-08-22 17:14:56 +0800

    GG

---
+++ TheKeyProject/Assets/Script/開關燈/Open_Light.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Open_Light : MonoBehaviour {
+
+	// Use this for initialization
+	public void Open()
+    {
+        
+    } 
+}
