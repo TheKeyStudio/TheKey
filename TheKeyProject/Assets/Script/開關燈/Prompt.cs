45c50873545080db74195aac074c5fba56d5376e
york <york125019@gmail.com>
2018-08-30 17:05:31 +0800

    測試黨的物品的對話

---
+++ TheKeyProject/Assets/Script/開關燈/Prompt.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Prompt : Interactable
+{               //提示圖案出現
+
+    public override void Init()
+    {
+        base.Init();
+        interactKey = KeyCode.Z;
+    }
+
+    public override void Interact()
+    {
+        base.Interact();
+    }
+}
