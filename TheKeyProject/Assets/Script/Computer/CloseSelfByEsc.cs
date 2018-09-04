bbce46a8168edc0289d606ef7e4360c81e7f4dcc
RayFV <rayfvfree@ouklook.com>
2018-09-04 13:49:46 +0800

    Terminal Updated

--- TheKeyProject/Assets/Script/Computer/CloseSelfByEsc.cs
+++ TheKeyProject/Assets/Script/Computer/CloseSelfByEsc.cs
@@ -3,19 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CloseSelfByEsc : MonoBehaviour {
-    Computer computer;
-    public GameObject computerGameObj;
-
-    private void Start()
-    {
-        computer = computerGameObj.GetComponent<Computer>();
-    }
 
     // Update is called once per frame
     void Update () {
         if (Input.GetKey(KeyCode.Escape) && isActiveAndEnabled)
         {
-            computer.Close();
+            gameObject.SetActive(false);
         }
 	}
 }
--- TheKeyProject/Assets/Script/Terminal/TerminalInput.cs
+++ TheKeyProject/Assets/Script/Terminal/TerminalInput.cs
@@ -11,8 +11,30 @@ public class TerminalInput : MonoBehaviour {
     
 	void Awake () {
         controller = GetComponent<TerminalController>();
-        inputField.onEndEdit.AddListener(AcceptStringInput);
+        inputField.onEndEdit.AddListener(FilterStringInput);
 	}
+
+    private void Update()
+    {
+        if (PressedEnterKey())
+        {
+            inputField.ActivateInputField();
+        }
+    }
+
+    void FilterStringInput(string userInput)
+    {
+        userInput = userInput.Trim();
+        if(userInput!="" && PressedEnterKey())
+        {
+            AcceptStringInput(userInput);
+        }
+    }
+
+    bool PressedEnterKey()
+    {
+        return Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown(KeyCode.KeypadEnter);
+    }
 
     void AcceptStringInput(string userInput)
     {
@@ -29,10 +51,6 @@ public class TerminalInput : MonoBehaviour {
             {
                 inputCmd.Respond(controller, separatedInputWords);
                 isRespond = true;
-            }
-            else
-            {
-                
             }
         }
 
