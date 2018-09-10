baaff0575a386116e1e438855e604120f52a9ef4
RayFV <rayfvfree@ouklook.com>
2018-09-10 22:47:10 +0800

    Added Text file font size and color

--- TheKeyProject/Assets/Script/Terminal/TerminalFilesHandler.cs
+++ TheKeyProject/Assets/Script/Terminal/TerminalFilesHandler.cs
@@ -50,5 +50,4 @@ public class TerminalFilesHandler : MonoBehaviour
         }
         return exist;
     }
-    
 }
--- TheKeyProject/Assets/Script/Terminal/TextFiles.cs
+++ TheKeyProject/Assets/Script/Terminal/TextFiles.cs
@@ -8,8 +8,17 @@ public class TextFiles : TerminalFiles
     [TextArea(10, 99)]
     public string content = "New Content";
 
+    [Range(0.1f, 2f)]
+    public float contentFontSizeMultiple = 0.7f;
+
+    public Color contentFontColor = new Color(0f, 1f, 0.5f);
+
     public override void Open(TerminalController controller)
     {
-        controller.LogString(content);
+        string richTextContext = "<#" + ColorUtility.ToHtmlStringRGB(contentFontColor) + ">" +
+            "<size=" + (contentFontSizeMultiple * 100f).ToString() + "%>" + content + 
+            "</size>" + "</color>";
+
+        controller.LogString(richTextContext);
     }
 }
