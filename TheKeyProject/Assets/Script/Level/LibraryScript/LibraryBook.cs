c76443e2f2d2af91e7096b66aeea35f11508f407
york <york125019@gmail.com>
2018-09-29 15:34:04 +0800

    主場景 靈魂對話完成

--- TheKeyProject/Assets/Script/Level/LibraryScript/LibraryBook.cs
+++ TheKeyProject/Assets/Script/Level/LibraryScript/LibraryBook.cs
@@ -4,34 +4,18 @@ using UnityEngine;
 using Fungus;
 using UnityEngine.SceneManagement;
 
-public class LibraryBook : KeyboardInteractable
+public class LibraryBook : MonoBehaviour
 {
     public Flowchart flowchart;
     public string nextScene;
-    // Use this for initialization
-    public override void Init()
-    {
-        base.Init();
-        interactKey = KeyCode.Z;
-    }
 
     // Update is called once per frame
-    public override void Update()
+    public void Update()
     {
         if (flowchart.GetBooleanVariable("isDone"))
         {
             SceneManager.LoadScene(nextScene);
         }
-        base.Update();
     }
-
-    public override void Interact()
-    {
-        base.Interact();
-        if (!flowchart.GetBooleanVariable("isDone"))
-        {
-            playerController.DeactiveMove();
-            Flowchart.BroadcastFungusMessage("書本劇情");
-        }
-    }
+    
 }
