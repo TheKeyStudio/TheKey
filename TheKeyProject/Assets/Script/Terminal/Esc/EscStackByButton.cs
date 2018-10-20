4829e25aa59f2ebee64cc8cd0ef3b98630864823
RayFV <rayfvfree@outlook.com>
2018-10-20 15:58:54 +0800

    Updated
    Added close by button
    Change file location

---
+++ TheKeyProject/Assets/Script/Terminal/Esc/EscStackByButton.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EscStackByButton : MonoBehaviour {
+
+    EscStack escStack;
+	// Use this for initialization
+	void Start () {
+        escStack = EscStack.instance;
+	}
+	
+	public void Close()
+    {
+        escStack.Pop();
+    }
+}
