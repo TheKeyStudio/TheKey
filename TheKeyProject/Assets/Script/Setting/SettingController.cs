4a62beba9d33d72afbf6be4fa77a7b813e6fcda5
RayFV <rayfvfree@outlook.com>
2018-11-04 15:23:44 +0800

    Added Setting UI and volume setter
    Don't know how to control the volume of fungus,
    need to make it self for audio control

---
+++ TheKeyProject/Assets/Script/Setting/SettingController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SettingController : MonoBehaviour {
+
+    private static bool GameIsPaused = false;
+
+    public GameObject menu;
+    public GameObject menuBtn;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            if (GameIsPaused)
+            {
+                CloseMenu();
+            }
+            else
+            {
+                OpenMenu();
+            }
+        }
+    }
+
+    public void OpenMenu()
+    {
+        menu.SetActive(true);
+        menuBtn.SetActive(false);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+
+    public void CloseMenu()
+    {
+        menu.SetActive(false);
+        menuBtn.SetActive(true);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+}
