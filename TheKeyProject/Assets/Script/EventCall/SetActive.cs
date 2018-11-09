e559ef92fa838a9c7a2b52134357d560f0ee24ab
RayFV <rayfvfree@outlook.com>
2018-11-09 13:46:19 +0800

    Added SetActive with EventCall

---
+++ TheKeyProject/Assets/Script/EventCall/SetActive.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetActive : MonoBehaviour {
+
+    public bool[] active;
+
+    private EventDataGetter eventDataGetter;
+
+    // Use this for initialization
+    void Awake()
+    {
+        eventDataGetter = GetComponent<EventDataGetter>();
+        Init();
+    }
+
+    private void Init()
+    {
+        int eventCode = eventDataGetter.GetData();
+        this.gameObject.SetActive(active[eventCode]);
+    }
+}
