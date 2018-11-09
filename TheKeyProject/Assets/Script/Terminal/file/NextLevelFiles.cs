2938923c2e4bec166dc3eed188bd2de1453356bb
RayFV <rayfvfree@outlook.com>
2018-11-09 14:47:50 +0800

    Updated
    Fixed SetActive and SetPosition Bug
    
    Added EventSetter to Terminal NextLevelFile

--- TheKeyProject/Assets/Script/Terminal/file/NextLevelFiles.cs
+++ TheKeyProject/Assets/Script/Terminal/file/NextLevelFiles.cs
@@ -5,20 +5,34 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "Terminal/Files/NextLevelFiles")]
 public class NextLevelFiles : TerminalFiles
 {
-    public int stage;
     public int openLevel;
+
+    [SerializeField] private int[] eventCodes;
+    [SerializeField] string[] eventNames;
 
     public override void Open(TerminalController controller)
     {
         GameManager gameManager = GameManager.instance;
         if (gameManager.GetCurrentLevel() < openLevel)
         {
+            SetData();
+
             gameManager.NextLevel();
             controller.LogString("Opened " + (gameManager.GetCurrentLevel()).ToString());
         }
         else
         {
             controller.LogString("Already Opened.");
+        }
+    }
+
+    private void SetData()
+    {
+        EventDataManager eventDataMgr;
+        eventDataMgr = GameManager.instance.EventDataManager;
+        for (int i = 0; i < eventCodes.Length; i++)
+        {
+            eventDataMgr.SetDataOrNew(eventNames[i], eventCodes[i]);
         }
     }
 }
