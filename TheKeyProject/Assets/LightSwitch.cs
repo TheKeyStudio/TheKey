91cf5cdd9769f6b057fb66f9366262b73e4fa43e
RayFV <rayfvfree@outlook.com>
2018-12-18 18:45:47 +0800

    Updated

---
+++ TheKeyProject/Assets/LightSwitch.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LightSwitch : MonoBehaviour {
+    private Button button;
+    public Sprite onImg;
+    public Sprite offImg;
+
+    public bool on = true;
+    public GameObject[] switchActiveObjs;
+
+    private void Start()
+    {
+        button = GetComponent<Button>();
+        SetImage();
+    }
+
+    public void Press()
+    {
+        on = !on;
+        SetImage();
+        SwitchAllObjsActive();
+    }
+
+    private void SwitchAllObjsActive()
+    {
+        foreach(GameObject obj in switchActiveObjs)
+        {
+            obj.SetActive(!obj.activeSelf);
+        }
+    }
+
+    public void SetImage()
+    {
+        if (on)
+        {
+            button.GetComponent<Image>().sprite = onImg;
+        }
+        else
+        {
+            button.GetComponent<Image>().sprite = offImg;
+        }
+    }
+}
